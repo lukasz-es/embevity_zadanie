633725eeaa2f9f0746b14633d9afc0d961c9041a
Lukasz Sokolowski <luke.sokolowski@gmail.com>
2025-08-24 14:31:13 +0200

    Add initial, untested emulated I2C implementation

--- i2c_emulated.c
+++ i2c_emulated.c
@@ -14,14 +14,23 @@ int i2c_registers_write(unsigned char registerAddr, unsigned char registersNo, u
 
 int i2c_initialize(void)
 {
-    if ( (queue_init(&write_queue_handle, QUEUE_PROJ_ID_WRITE)) || (queue_init(&read_queue_handle, QUEUE_PROJ_ID_READ)) )
+    if ( (queue_init(&write_queue_handle, QUEUE_PROJ_ID_WRITE) != I2C_SUCCESS) || (queue_init(&read_queue_handle, QUEUE_PROJ_ID_READ) != I2C_SUCCESS) )
     {
         return I2C_CREATE_FAILED;
     }
     return I2C_SUCCESS;
 }
 
-static int queue_init(IPCMessageQueueHandles *handles, int queue_proj_id)
+int i2c_close(void)
+{
+    if ( (queue_destroy(&write_queue_handle) != I2C_SUCCESS) || (queue_destroy(&read_queue_handle) != I2C_SUCCESS) )
+    {
+        return I2C_DESTROY_FAILED;
+    }
+    return I2C_SUCCESS;
+}
+
+static inline int queue_init(IPCMessageQueueHandles *handles, int queue_proj_id)
 {
     /* TODO: error checks */
     handles->queue_key = ftok(QUEUE_ID_FILE, queue_proj_id);
@@ -30,17 +39,61 @@ static int queue_init(IPCMessageQueueHandles *handles, int queue_proj_id)
     return I2C_SUCCESS;
 }
 
-static int write_queue_send(unsigned char opType, unsigned char *data)
+static inline int queue_destroy(IPCMessageQueueHandles *handles)
 {
-    return I2C_NOT_IMPLEMENTED_YET;
+    msgctl(handles->queue_id, IPC_RMID, NULL);
+    handles->queue_initialized = 0;
+    return I2C_SUCCESS;
 }
 
-static int read_queue_init(void)
+static inline int write_queue_send(unsigned char opType, int dataLen, unsigned char *data)
 {
-    return I2C_NOT_IMPLEMENTED_YET;
+    if (!write_queue_handle.queue_initialized)
+    {
+        return I2C_WRITE_FAILED;
+    }
+
+    if (dataLen > MAX_IPC_DATA_SIZE)
+    {
+        return I2C_WRITE_FAILED;
+    }
+
+    IPCMessage msg;
+    msg.messageType = QUEUE_IPC_MSG_TYPE;
+    msg.data[0] = opType;
+    msg.data[1] = dataLen;
+    memcpy(&msg.data[2], data, dataLen);
+
+    if (msgsnd(write_queue_handle.queue_id, &msg, sizeof(msg), QUEUE_IPC_FLAGS) != 0)
+    {
+        return I2C_WRITE_FAILED;
+    }
+
+    return I2C_SUCCESS;
 }
 
-static int read_queue_receive(unsigned char opType, unsigned char *data)
+static inline int read_queue_receive(unsigned char *opType, int *dataLen, unsigned char *data)
 {
-    return I2C_NOT_IMPLEMENTED_YET;
+    if (!read_queue_handle.queue_initialized)
+    {
+        return I2C_READ_FAILED;
+    }
+
+    IPCMessage msg;
+
+    if (msgrcv(read_queue_handle.queue_id, &msg, sizeof(msg), QUEUE_IPC_MSG_TYPE, IPC_RECEIVE_FLAGS) < 0)
+    {
+        return I2C_READ_FAILED;
+    }
+
+    if (msg.data[1] > MAX_IPC_DATA_SIZE)
+    {
+        return I2C_READ_FAILED;
+    }
+
+    *opType=msg.data[0];
+    *dataLen=msg.data[1];
+    memcpy(data, &data[2], *dataLen);
+
+    return I2C_SUCCESS;
 }
--- i2c_emulated.h
+++ i2c_emulated.h
@@ -6,8 +6,9 @@
 extern "C" {
 #endif
 
-#include <sys/ipc.h>
-#include <sys/msg.h>
+#include <sys/ipc.h> /* For RPC message queue */
+#include <sys/msg.h> /* For RPC message queue */
+#include <string.h> /* For memcpy */
 
 /* Error definitions */
 #define I2C_ERROR_BASE -10
@@ -15,28 +16,34 @@ extern "C" {
 #define I2C_WRITE_FAILED (I2C_ERROR_BASE-0)
 #define I2C_READ_FAILED (I2C_ERROR_BASE-1)
 #define I2C_CREATE_FAILED (I2C_ERROR_BASE-2)
-#define I2C_NOT_IMPLEMENTED_YET (I2C_ERROR_BASE-3)
+#define I2C_DESTROY_FAILED (I2C_ERROR_BASE-3)
+#define I2C_NOT_IMPLEMENTED_YET (I2C_ERROR_BASE-4)
 
 int i2c_initialize(void);
+int i2c_close(void);
 int i2c_registers_read(unsigned char registerAddr, unsigned char registersNo, unsigned char *data);
 int i2c_registers_write(unsigned char registerAddr, unsigned char registersNo, unsigned char *data);
 
 /* IPC queues definitions */
 #define IPC_MSG_TYPE_READ 1
 #define IPC_MSG_TYPE_WRITE 2
+#define QUEUE_IPC_MSG_TYPE 1
 
 #define QUEUE_ID_FILE "queueidfile"
 #define QUEUE_ACCESS_MOD 0666
+#define QUEUE_IPC_FLAGS 0
 #define QUEUE_PROJ_ID_READ 12
 #define QUEUE_PROJ_ID_WRITE 34
 
 #define MAX_IPC_DATA_SIZE 100
+#define IPC_HEADER_SIZE 2
+#define MAX_IPC_TOTAL_SIZE (MAX_IPC_DATA_SIZE+IPC_HEADER_SIZE)
+#define IPC_RECEIVE_FLAGS 0
 
 typedef struct
 {
-    int messageLen; /* Data buffer length in bytes */
-    int messageType;
-    unsigned char data[MAX_IPC_DATA_SIZE];
+    long messageType;
+    unsigned char data[MAX_IPC_TOTAL_SIZE];
 } IPCMessage;
 
 typedef struct
@@ -47,9 +54,10 @@ typedef struct
 } IPCMessageQueueHandles;
 
 /* Internal emulator functions definitions */
-static int queue_init(IPCMessageQueueHandles *handles, int queue_proj_id);
-static int write_queue_send(unsigned char opType, unsigned char *data);
-static int read_queue_receive(unsigned char opType, unsigned char *data);
+static inline int queue_init(IPCMessageQueueHandles *handles, int queue_proj_id);
+static inline int queue_destroy(IPCMessageQueueHandles *handles);
+static inline int write_queue_send(unsigned char opType, int dataLen, unsigned char *data);
+static inline int read_queue_receive(unsigned char *opType, int *dataLen, unsigned char *data);
 
 /* End C++ header guard */
 #ifdef __cplusplus
