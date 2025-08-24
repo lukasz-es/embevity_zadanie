eb18dbd90ba218a31e64808bad41f03508f4f40a
Lukasz Sokolowski <luke.sokolowski@gmail.com>
2025-08-24 13:24:33 +0200

    Add I2C emulation queue init

--- i2c_emulated.c
+++ i2c_emulated.c
@@ -1,11 +1,46 @@
 #include "i2c_emulated.h"
 
+static IPCMessageQueueHandles write_queue_handle, read_queue_handle;
+
 int i2c_registers_read(unsigned char registerAddr, unsigned char registersNo, unsigned char *data)
 {
     return I2C_NOT_IMPLEMENTED_YET;
 }
 
 int i2c_registers_write(unsigned char registerAddr, unsigned char registersNo, unsigned char *data)
+{
+    return I2C_NOT_IMPLEMENTED_YET;
+}
+
+int i2c_initialize(void)
+{
+    if ( (queue_init(&write_queue_handle, QUEUE_PROJ_ID_WRITE)) || (queue_init(&read_queue_handle, QUEUE_PROJ_ID_READ)) )
+    {
+        return I2C_CREATE_FAILED;
+    }
+    return I2C_SUCCESS;
+}
+
+static int queue_init(IPCMessageQueueHandles *handles, int queue_proj_id)
+{
+    /* TODO: error checks */
+    handles->queue_key = ftok(QUEUE_ID_FILE, queue_proj_id);
+    handles->queue_id = msgget(handles->queue_key, (QUEUE_ACCESS_MOD | IPC_CREAT));
+    handles->queue_initialized=1;
+    return I2C_SUCCESS;
+}
+
+static int write_queue_send(unsigned char opType, unsigned char *data)
+{
+    return I2C_NOT_IMPLEMENTED_YET;
+}
+
+static int read_queue_init(void)
+{
+    return I2C_NOT_IMPLEMENTED_YET;
+}
+
+static int read_queue_receive(unsigned char opType, unsigned char *data)
 {
     return I2C_NOT_IMPLEMENTED_YET;
 }
--- i2c_emulated.h
+++ i2c_emulated.h
@@ -1,12 +1,59 @@
 #ifndef I2C_EMULATED_H
 #define I2C_EMULATED_H
 
+/* C++ header guard */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+/* Error definitions */
 #define I2C_ERROR_BASE -10
+#define I2C_SUCCESS 0
 #define I2C_WRITE_FAILED (I2C_ERROR_BASE-0)
 #define I2C_READ_FAILED (I2C_ERROR_BASE-1)
-#define I2C_NOT_IMPLEMENTED_YET (I2C_ERROR_BASE-2)
+#define I2C_CREATE_FAILED (I2C_ERROR_BASE-2)
+#define I2C_NOT_IMPLEMENTED_YET (I2C_ERROR_BASE-3)
 
+int i2c_initialize(void);
 int i2c_registers_read(unsigned char registerAddr, unsigned char registersNo, unsigned char *data);
 int i2c_registers_write(unsigned char registerAddr, unsigned char registersNo, unsigned char *data);
+
+/* IPC queues definitions */
+#define IPC_MSG_TYPE_READ 1
+#define IPC_MSG_TYPE_WRITE 2
+
+#define QUEUE_ID_FILE "queueidfile"
+#define QUEUE_ACCESS_MOD 0666
+#define QUEUE_PROJ_ID_READ 12
+#define QUEUE_PROJ_ID_WRITE 34
+
+#define MAX_IPC_DATA_SIZE 100
+
+typedef struct
+{
+    int messageLen; /* Data buffer length in bytes */
+    int messageType;
+    unsigned char data[MAX_IPC_DATA_SIZE];
+} IPCMessage;
+
+typedef struct
+{
+    key_t queue_key;
+    int queue_id;
+    int queue_initialized;
+} IPCMessageQueueHandles;
+
+/* Internal emulator functions definitions */
+static int queue_init(IPCMessageQueueHandles *handles, int queue_proj_id);
+static int write_queue_send(unsigned char opType, unsigned char *data);
+static int read_queue_receive(unsigned char opType, unsigned char *data);
+
+/* End C++ header guard */
+#ifdef __cplusplus
+}
+#endif
 
 #endif // I2C_EMULATED_H
