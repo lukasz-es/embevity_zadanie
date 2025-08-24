799633882cf1633238761666e962903b96cbb1c3
Lukasz Sokolowski <luke.sokolowski@gmail.com>
2025-08-24 12:09:45 +0200

    Refactor error definitions

--- accelerometer.c
+++ accelerometer.c
@@ -2,25 +2,25 @@
 
 int startAcquisition(void)
 {
-    return NOT_IMPLEMENTED_YET;
+    return ACCEL_NOT_IMPLEMENTED_YET;
 }
 
 int stopAcquisition(void)
 {
-    return NOT_IMPLEMENTED_YET;
+    return ACCEL_NOT_IMPLEMENTED_YET;
 }
 
 int accelSetConfig(AccelConfigData *config)
 {
-    return NOT_IMPLEMENTED_YET;
+    return ACCEL_NOT_IMPLEMENTED_YET;
 }
 
 int gyroSetConfig(GyroConfigData *config)
 {
-    return NOT_IMPLEMENTED_YET;
+    return ACCEL_NOT_IMPLEMENTED_YET;
 }
 
 int getData(AccelData *data)
 {
-    return NOT_IMPLEMENTED_YET;
+    return ACCEL_NOT_IMPLEMENTED_YET;
 }
