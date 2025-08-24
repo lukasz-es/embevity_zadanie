114f8b29e9fe35956556a46abb06779dfb49d5f8
Lukasz Sokolowski <luke.sokolowski@gmail.com>
2025-08-24 11:27:03 +0200

    Add IMU API draft

---
+++ accelerometer.c
@@ -0,0 +1,26 @@
+#include "accelerometer.h"
+
+int startAcquisition(void)
+{
+    return NOT_IMPLEMENTED_YET;
+}
+
+int stopAcquisition(void)
+{
+    return NOT_IMPLEMENTED_YET;
+}
+
+int accelSetConfig(AccelConfigData *config)
+{
+    return NOT_IMPLEMENTED_YET;
+}
+
+int gyroSetConfig(GyroConfigData *config)
+{
+    return NOT_IMPLEMENTED_YET;
+}
+
+int getData(AccelData *data)
+{
+    return NOT_IMPLEMENTED_YET;
+}
---
+++ accelerometer.h
@@ -0,0 +1,48 @@
+#ifndef ACCELEROMETER_H
+#define ACCELEROMETER_H
+
+#include "ICM_42670_defs.h"
+
+#define ACCEL_ERROR_NONE 0
+
+#define ACCEL_ERROR_BASE -1
+
+#define ACCEL_ERROR_I2C_FAIL (ACCEL_ERROR_BASE-0)
+#define ACCEL_ERROR_NOT_CONFIGURED (ACCEL_ERROR_BASE-1)
+#define ACCEL_ERROR_INVALID_CFG_VALUE (ACCEL_ERROR_BASE-2)
+#define ACCEL_ERROR_OTHER (ACCEL_ERROR_BASE-3)
+#define ACCEL_NOT_IMPLEMENTED_YET (ACCEL_ERROR_BASE-4)
+
+typedef struct
+{
+    /* Per axis values */
+    float x;
+    float y;
+    float z;
+} AxesData;
+
+typedef struct
+{
+    AxesData gyro; /* Gyroscope data, in dps units */
+    AxesData accel; /* Accelerometer data, in g units */
+} AccelData;
+
+typedef struct
+{
+    ICM_42670_GyroRange range; /* Measurements range */
+    ICM_42670_GyroFreq freq;
+} GyroConfigData;
+
+typedef struct
+{
+    ICM_42670_AccelRange range;
+    ICM_42670_AccelFreq freq;
+} AccelConfigData;
+
+int startAcquisition(void);
+int stopAcquisition(void);
+int accelSetConfig(AccelConfigData *config);
+int gyroSetConfig(GyroConfigData *config);
+int getData(AccelData *data);
+
+#endif // ACCELEROMETER_H
