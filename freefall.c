973918150093307889ccdd873eea997187402a13
Lukasz Sokolowski <luke.sokolowski@gmail.com>
2025-08-25 16:10:37 +0200

    Add initial freefall implementation

--- freefall.c
+++ freefall.c
@@ -1,2 +1,31 @@
 #include "freefall.h"
 
+void freefall_init(FreeFallDataCache *cache, int min_samples, float threshold)
+{
+	cache->already_triggered = 0;
+	cache->min_samples_no = min_samples;
+	cache->samples_buffer = 0;
+	cache->samples_buffer_mask = frontFill(sizeof(unsigned long int)-min_samples);
+	cache->threshold = threshold;
+}
+
+int freefall_check(FreeFallDataCache *cache, AccelData *data)
+{
+	return 0;
+}
+
+unsigned long int frontFill(int bits)
+{
+	unsigned long int result=1;
+	int i;
+
+    for (i=0; i<(bits - 1); i++)
+    {
+        result <<=1;
+        result|=1;
+    }
+
+    result=~result;
+	
+	return result;
+}
--- freefall.h
+++ freefall.h
@@ -2,5 +2,19 @@
 #define FREEFALL_H
 
 #include "accelerometer.h"
+#include <math.h>
+
+typedef struct
+{
+	int already_triggered;
+	int min_samples_no;
+	unsigned long int samples_buffer;
+	unsigned long int samples_buffer_mask;
+	float threshold;
+} FreeFallDataCache;
+
+void freefall_init(FreeFallDataCache *cache, int min_samples, float threshold);
+int freefall_check(FreeFallDataCache *cache, AccelData *data);
+unsigned long int frontFill(int bits);
 
 #endif // FREEFALL_H
