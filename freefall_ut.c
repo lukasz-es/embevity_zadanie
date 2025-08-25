0c3f6d623467e483947f3cc489ae804a9db9b104
Lukasz Sokolowski <luke.sokolowski@gmail.com>
2025-08-25 17:50:59 +0200

    Add initial UT battery

--- freefall_ut.c
+++ freefall_ut.c
@@ -1,12 +1,162 @@
 #include "utest.h"
+#include "accelerometer.h"
+#include "freefall.h"
 
 UTEST_STATE();
 
-UTEST(foo, bar) {
-  ASSERT_TRUE(1);
+FreeFallDataCache cache;
+AccelData data;
+
+UTEST(cache, init) 
+{
+  freefall_init(&cache, 3, 1.0);
+  ASSERT_EQ(0, cache.already_triggered);
+  ASSERT_EQ(3, cache.min_samples_no);
+  ASSERT_EQ(1.0, cache.threshold);
+  ASSERT_EQ(0, cache.samples_buffer);
+  ASSERT_NE(0, cache.samples_buffer_mask);
 }
 
-int main(int argc, const char *const argv[]) {
-  // do your own thing
+/* All axes above trigger level */
+UTEST(freefall, no_trigger)
+{
+	freefall_init(&cache, 3, 1.0);
+	
+	data.accel.x = 1.5;
+	data.accel.y = 1.5;
+	data.accel.z = 1.5;
+	
+	int retval, i;
+	
+	for (i=0; i<10; i++)
+	{
+		retval = freefall_check(&cache, &data);
+		ASSERT_EQ(0, retval);
+	}
+}
+
+/* All axes below trigger level */
+UTEST(freefall, trigger)
+{
+	freefall_init(&cache, 3, 1.0);
+	
+	data.accel.x = 0.5;
+	data.accel.y = 0.5;
+	data.accel.z = 0.5;
+	
+	int retval;
+	retval = freefall_check(&cache, &data);
+	ASSERT_EQ(0, retval);
+	retval = freefall_check(&cache, &data);
+	ASSERT_EQ(0, retval);
+	retval = freefall_check(&cache, &data);
+	ASSERT_EQ(1, retval);
+	retval = freefall_check(&cache, &data);
+	ASSERT_EQ(1, retval);
+	retval = freefall_check(&cache, &data);
+	ASSERT_EQ(1, retval);
+}
+
+/* One axis above trigger level */
+UTEST(freefall, 1_above)
+{
+	freefall_init(&cache, 3, 1.0);
+	
+	data.accel.x = 1.5;
+	data.accel.y = 0.5;
+	data.accel.z = 0.5;
+	
+	int retval, i;
+	
+	for (i=0; i<10; i++)
+	{
+		retval = freefall_check(&cache, &data);
+		ASSERT_EQ(0, retval);
+	}
+}
+
+/* Two axes above trigger level */
+UTEST(freefall, 2_above)
+{
+	freefall_init(&cache, 3, 1.0);
+	
+	data.accel.x = 1.5;
+	data.accel.y = 0.5;
+	data.accel.z = 1.5;
+	
+	int retval, i;
+	
+	for (i=0; i<10; i++)
+	{
+		retval = freefall_check(&cache, &data);
+		ASSERT_EQ(0, retval);
+	}
+}
+
+/* Triggered fall is sustained */
+UTEST(freefall, sustain)
+{
+	freefall_init(&cache, 3, 1.0);
+	
+	data.accel.x = 0.5;
+	data.accel.y = 0.5;
+	data.accel.z = 0.5;
+	
+	int retval, i;
+	retval = freefall_check(&cache, &data);
+	ASSERT_EQ(0, retval);
+	retval = freefall_check(&cache, &data);
+	ASSERT_EQ(0, retval);
+	retval = freefall_check(&cache, &data);
+	ASSERT_EQ(1, retval);
+	retval = freefall_check(&cache, &data);
+	ASSERT_EQ(1, retval);
+	retval = freefall_check(&cache, &data);
+	ASSERT_EQ(1, retval);
+	
+	data.accel.x = 1.5;
+	data.accel.y = 1.5;
+	data.accel.z = 1.5;
+	
+	for (i=0; i<5; i++)
+	{
+		retval = freefall_check(&cache, &data);
+		ASSERT_EQ(1, retval);
+	}
+}
+
+/* Erratic data won't trigger detection */
+UTEST(freefall, erratic)
+{
+	int retval, i;
+		
+	freefall_init(&cache, 3, 1.0);
+	
+	for (i=0; i<5; i++)
+	{
+		data.accel.x = 0.5;
+		data.accel.y = 0.5;
+		data.accel.z = 0.5;
+		
+		retval = freefall_check(&cache, &data);
+		ASSERT_EQ(0, retval);
+		retval = freefall_check(&cache, &data);
+		ASSERT_EQ(0, retval);
+		
+		data.accel.x = 1.5;
+		data.accel.y = 1.5;
+		data.accel.z = 1.5;
+		
+		retval = freefall_check(&cache, &data);
+		ASSERT_EQ(0, retval);
+		retval = freefall_check(&cache, &data);
+		ASSERT_EQ(0, retval);
+	}
+	
+}
+
+int main(int argc, const char *const argv[])
+{
+	
   return utest_main(argc, argv);
 }
